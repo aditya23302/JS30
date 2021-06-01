97817e6d921811cad4d1808506ea40a1484fa2f6
unknown <adityakupa1999@gmail.com>
2021-06-01 21:04:47 +0530

    Analog-Clock Created

---
+++ Analogue-Clock/app.js
@@ -0,0 +1,25 @@
+const hourHand=document.querySelector('.hour-hand');
+const secHand=document.querySelector('.sec-hand');
+const minHand=document.querySelector('.min-hand');
+
+function setDate()
+{
+    const now=new Date();
+
+    const seconds=now.getSeconds();
+    const sDegree=((seconds/60)*360)+90;
+    secHand.style.transform=`rotate(${sDegree}deg)`;
+
+    const minutes=now.getMinutes();
+    const mDegree=((minutes/60)*360)+90;
+    minHand.style.transform=`rotate(${mDegree}deg)`;
+
+    // const hours=now.getHours();
+    // const hDegree=((hours/12)*360)+90;
+    // hourHand.style.transform=`rotate(${hDegree}deg)`;
+
+
+}
+setInterval(setDate,1000);
+setDate();
+
