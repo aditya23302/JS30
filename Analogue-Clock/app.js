e6eea23f4a5d9888097c2f1a8d2ca082c4457ac1
unknown <adityakupa1999@gmail.com>
2021-06-02 08:47:33 +0530

    Modified Analog Clock

--- Analogue-Clock/app.js
+++ Analogue-Clock/app.js
@@ -11,11 +11,11 @@ function setDate()
     secHand.style.transform=`rotate(${sDegree}deg)`;
 
     const minutes=now.getMinutes();
-    const mDegree=((minutes/60)*360)+90;
+    const mDegree=((minutes/60)*360)+((seconds/60)*6) + 90;
     minHand.style.transform=`rotate(${mDegree}deg)`;
 
      const hours=now.getHours();
-     const hDegree=((hours/12)*360)+90;
+     const hDegree=((hours/12)*360)+ ((minutes/60)*30) + 90;
      hourHand.style.transform=`rotate(${hDegree}deg)`;
 
 
